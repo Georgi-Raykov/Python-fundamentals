6a569b4154a1f22e479d8b362bac543a0ce72c50
Georgi <112810653+Gogo8618@users.noreply.github.com>
2024-05-08 14:43:58 +0300

    Data Types and Variables

---
+++ Special number.py
@@ -0,0 +1,14 @@
+number = int(input())
+
+for n in range(1, number + 1):
+
+    working_number = n
+    sum_digit = 0
+
+    while working_number > 0:
+
+        sum_digit += working_number % 10
+        working_number = int(working_number / 10)
+
+    special_number = sum_digit == 5 or sum_digit == 7 or sum_digit == 11
+    print(n)
