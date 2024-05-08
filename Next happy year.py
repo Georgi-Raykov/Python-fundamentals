57fcb7669d51874f9324dd8d374836d08a1dded7
Georgi <112810653+Gogo8618@users.noreply.github.com>
2024-05-08 16:01:03 +0300

    Data Types and Variables

---
+++ Next happy year.py
@@ -0,0 +1,12 @@
+year = int(input())
+is_happy = False
+while not is_happy:
+    year += 1
+    str_year = str(year)
+    set_year = set(str_year)
+    is_happy = len(set_year) == len(str_year)
+
+
+print(year)
+
+
