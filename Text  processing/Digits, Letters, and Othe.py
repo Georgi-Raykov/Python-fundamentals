708f7ab946406450f9196b7ea3c8288c777b30b2
Georgi Raykov <graykov7@gmail.com>
2024-06-17 16:43:54 +0300

    Text processing

---
+++ Text  processing/Digits, Letters, and Othe.py
@@ -0,0 +1,18 @@
+text = input()
+
+letters = ''
+digits = ''
+symbols = ''
+
+for ch in text:
+
+    if ch.isdigit():
+        digits += ch
+    elif ch.isalpha():
+        letters += ch
+    else:
+        symbols += ch
+
+print(digits)
+print(letters)
+print(symbols)
